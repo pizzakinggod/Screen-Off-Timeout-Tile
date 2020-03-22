78bbed0843a40d4a09691f77a5945d3197612828
pizzakinggod <mgparkprint@gmail.com>
2020-03-22 20:25:52 +0900

    Change default value for preferences

--- app/src/main/java/com/instantpood/screenofftimeout/MainActivity.java
+++ app/src/main/java/com/instantpood/screenofftimeout/MainActivity.java
@@ -1,14 +1,21 @@
 package com.instantpood.screenofftimeout;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        setContentView(R.layout.activity_main); //먼저 레이아웃을 주고
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.settings_container, new SettingsFragment())
+                .commit(); // 안에있는 fragment를 대체
     }
+
+
 }
